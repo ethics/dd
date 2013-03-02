c2eacb026613c24f7769e0991b217a68da0e82df
siva <sivakumar.bs@gmail.com>
2013-03-02 18:00:21 +0530

    Adding Major and Minor Number

--- ofd/ofd.c
+++ ofd/ofd.c
@@ -2,15 +2,26 @@
 #include<linux/module.h>
 #include<linux/version.h>
 #include<linux/kernel.h>
+#include<linux/types.h>
+#include<linux/kdev_t.h>
+#include<linux/fs.h>
+
+static dev_t first; //Global variablr for the first device number
 
 static int __init ofd_init(void) /*Constructor */
 {
 	printk(KERN_INFO "Hi: ofd registered");
+     	if(alloc_chrdev_region(&first, 0, 3, "Siva") < 0)
+	{
+	  return -1;
+	}
+	printk(KERN_INFO "<Major, Minor>: <%d, %d>\n", MAJOR(first), MINOR(first));
 	return 0;
 }
 
 static void __exit ofd_exit(void) /*Destructor*/
 {
+        unregister_chrdev_region(first, 3);
 	printk(KERN_INFO "Bye: ofd unregistered");
 }
 
